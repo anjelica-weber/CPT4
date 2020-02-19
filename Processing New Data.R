e629e54520513ff1321b77a8ecd634e77d25623e
anjelica-weber <anjelica.weber@mssm.edu>
2020-02-19 16:23:56 -0500

    Added alternative error handling in the Importing data code section, must test under different conditions

--- Processing New Data.R
+++ Processing New Data.R
@@ -9,8 +9,8 @@ folder_RAWdata <- choose.dir(caption = "Select folder with RAW CPT Data")
 list_filenames_RAWdata <- list.files(path = folder_RAWdata, full.names = T, pattern = "csv$") # only pulls in .csv files
 CPT_data_list <- lapply(list_filenames_RAWdata, read.csv)
   # Quality Check - correct number of files loaded
-  stopifnot(length(CPT_data_list) == 3) # run code again, make sure the correct folder is selected
-  
+  stopifnot(length(CPT_data_list) == 3)
+  #tryCatch(stopifnot(length(CPT_data_list) == 3), error= stop("Unexpected number of files in selected folder"))  # check to see if this works, more user friendly
 
 # Pre Processing Data -----------------------------------------------------
 # Converting csv files into 1 table
@@ -50,4 +50,4 @@ range_master_data <- range(master_data_RAW$PostingDate)
 tryCatch(stopifnot(exprs = {
   !range_new_data[1]< range_master_data[2]
   !range_new_data[2]< range_master_data[2]
-}), error= stop("Range of the new data has already been added to the Master RAW Database"))
+}), error= stop("Range of the new data has already been added to the Master RAW Database")) # test this with different conditions
