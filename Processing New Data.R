ace7c4228088f880d82d4456d321ade2cfea1cd5
anjelica-weber <anjelica.weber@mssm.edu>
2020-02-19 15:42:18 -0500

    Sorting RAW data based on the columns, added in comments to explain what is happening in the Pre Processing Data section

--- Processing New Data.R
+++ Processing New Data.R
@@ -13,6 +13,7 @@ CPT_data_list <- lapply(list_filenames_RAWdata, read.csv)
   
 
 # Pre Processing Data -----------------------------------------------------
+# Converting csv files into 1 table
 CPT_data_list <- lapply(CPT_data_list, function(x) cSplit(x, colnames(x), sep = '|', type.convert = T)) # separating csv into table
   col_names_split <- function(df){
     new_col_names<- unlist(stri_split_fixed(str = colnames(df)[1], '.'))
@@ -29,12 +30,13 @@ CPT_data_list <- lapply(CPT_data_list, function(x) col_names_split(x)) # definin
   return(output)
 }
 CPT_data_RAW <- merge_multiple_dataframes(CPT_data_list)
-  #Formatting Date Columns
-  CPT_data_RAW$PostingDate<- as.Date(CPT_data_RAW$PostingDate)
-  CPT_data_RAW$ServiceDate<- as.Date(CPT_data_RAW$ServiceDate)
+#Formatting Date Columns
+CPT_data_RAW$PostingDate<- as.Date(CPT_data_RAW$PostingDate)
+CPT_data_RAW$ServiceDate<- as.Date(CPT_data_RAW$ServiceDate)
+#Sorting Data
+CPT_data_RAW<- arrange(CPT_data_RAW,`PostingDate`,`FacilityId` ,`RevenueCenter` , `ChargeCode`)
 
 
-
-# Importing Master Data Base --------------------------------------------
-path_master_data_RAW <- choose.files(caption = "Select Master RAW Data Base File", multi = F)
+# Importing Master Database --------------------------------------------
+path_master_data_RAW <- choose.files(caption = "Select Master RAW Database File", multi = F)
 master_data_RAW<- read.csv(path_master_data_RAW)
