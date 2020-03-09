2f5de9ae423769e92a94bfb073a82531c268a519
anjelica-weber <anjelica.weber@mssm.edu>
2020-03-09 11:17:29 -0400

    Working on function to generate Premier Upload file

--- Pulling Premier File from Master.R
+++ Pulling Premier File from Master.R
@@ -1,5 +1,5 @@
 # Loading Libraries -------------------------------------------------------
-
+library(xlsx)
 
 # Importing Master Data File ----------------------------------------------
 if (exists('master_data_RAW')==F) {
@@ -9,4 +9,26 @@ if (exists('master_data_RAW')==F) {
   master_data_RAW$PostingDate <- as.Date(master_data_RAW$PostingDate)
   master_data_RAW$ServiceDate <- as.Date(master_data_RAW$ServiceDate)
 }#if the master data isn't already loaded in the environment import data
+
+# Creating Premier Upload File --------------------------------------------
+generate_Premier_file <- function(df,start_date,end_date){
+  start_date <- as.Date(start_date, tryFormats= c("%m/%d/%Y", '%m-%d-%Y', '%m/%d/%y', '%m-%d-%y'))
+  end_date <- as.Date(end_date, tryFormats= c("%m/%d/%Y", '%m-%d-%Y', '%m/%d/%y', '%m-%d-%y'))
+  premier_data <- df[((df$ServiceDate >= start_date) & (df$ServiceDate <= end_date)),] #subset of data by date range of service
+  #Importing Dictionaries
+    #Charge Code to CPT Code "Charge Description Master"
+    cat("Select the most recent Charge Description Master", fill = T)
+    path_dict_CDM <- file.choose(new = T)
+    dict_CDM <- read.csv(file = path_dict_CDM, na.strings = c("", "Unavailable"))
+    dict_CDM <- subset(dict_CDM, select = c("CHARGE_CODE", "IPTB_cpt4"))
+    #colnames(dict_cdm)<- c("ChargeCode", "CPTCode")
+    #Revenue to Cost Center 
+    cat("Select the Revenue to Cost Center Mapping file", fill = T)
+    path_dict_CC<- file.choose(new = T)
+    dict_CC<- read.xlsx(path_dict_CC, sheetIndex = 1)
+  #Merging dictionaries and data
+    premier_data <- merge.data.frame(premier_data, dict_CC, all.x = T, all.y = F)
+    premier_data <- merge.data.frame(premier_data, dict_CDM, all.x = T, all.y = F)
+  return(premier_data)
+} 
 
