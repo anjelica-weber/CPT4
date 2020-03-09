d95c04c53635f6f663974145c617f8ae44de2bd9
anjelica-weber <anjelica.weber@mssm.edu>
2020-03-09 16:39:32 -0400

    Finished function to generate export file, and create the code section to export the data

--- Pulling Premier File from Master.R
+++ Pulling Premier File from Master.R
@@ -1,5 +1,6 @@
 # Loading Libraries -------------------------------------------------------
 library(xlsx)
+library(dplyr)
 
 # Importing Master Data File ----------------------------------------------
 if (exists('master_data_RAW')==F) {
@@ -32,6 +33,22 @@ generate_Premier_file <- function(df,start_date,end_date){
   #Merging dictionaries and data
     premier_data <- merge.data.frame(premier_data, dict_CC, all.x = T, all.y = F)
     premier_data <- merge.data.frame(premier_data, dict_CDM, all.x = T, all.y = F)
+  #Aggregating Data
+    premier_data <- aggregate(premier_data$NumberOfUnits, by= list(premier_data$Premier.Facility.ID, premier_data$Cost.Center,premier_data$ServiceDate, premier_data$CPTCode), FUN= 'sum')
+    colnames(premier_data) <- c('Facility ID', 'Cost Center', 'Start Date', 'CPT4 Code', 'Volume' )
+  # Processing Data
+    premier_data <- arrange(premier_data,`Facility ID`, `Start Date`, `Cost Center`, `CPT4 Code` )
+    premier_data$`Budget` <- rep(0, length(premier_data$`Facility ID`))
+    premier_data$`End Date` <- premier_data$`Start Date`
+    premier_data$`Corporate ID` <- rep(729805, length(premier_data$`Facility ID`))
+    premier_data <- premier_data[, c(8,1,2,3,7,4,5,6)] #correct column order
   return(premier_data)
 } 
+data_Premier <- generate_Premier_file(master_data_RAW, readline(prompt = "Start Date of Pay Period needed(mm/dd/yyyy):"),readline(prompt = "End date of Pay Period needed(mm/dd/yyyy):") )
 
+# Exporting Premier Files -------------------------------------------------
+#data_west <- data_dates_needed[(data_dates_needed$`Facility ID`=="NY2162"),]
+#data_lukes <- data_dates_needed[(data_dates_needed$`Facility ID`=="NY2163"),]
+#write.table(data_west, file ="MSW_CPT4_1AUG19 to 31DEC19.csv",row.names = F, col.names = T, sep = ',')
+#write.table(data_lukes, file ="MSSL_CPT4_1AUG19 to 31DEC19.csv",row.names = F, col.names = T, sep = ',')
+write.table(data_Premier, file = "test_full master.csv", row.names = F, col.names = T, sep = ',')
