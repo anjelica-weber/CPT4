b40613087a1fe1c9077c7694e4cd24474636e0a3
anjelica-weber <anjelica.weber@mssm.edu>
2020-03-09 11:57:53 -0400

    Fixed the bug, now able to merge CDM with the master data.

--- Pulling Premier File from Master.R
+++ Pulling Premier File from Master.R
@@ -14,7 +14,9 @@ if (exists('master_data_RAW')==F) {
 generate_Premier_file <- function(df,start_date,end_date){
   start_date <- as.Date(start_date, tryFormats= c("%m/%d/%Y", '%m-%d-%Y', '%m/%d/%y', '%m-%d-%y'))
   end_date <- as.Date(end_date, tryFormats= c("%m/%d/%Y", '%m-%d-%Y', '%m/%d/%y', '%m-%d-%y'))
+  #Subsetting Data
   premier_data <- df[((df$ServiceDate >= start_date) & (df$ServiceDate <= end_date)),] #subset of data by date range of service
+  premier_data <- subset(premier_data, select = c("FacilityId", "RevenueCenter" ,"ChargeCode", "ServiceDate" ,"NumberOfUnits"))
   #Importing Dictionaries
     #Charge Code to CPT Code "Charge Description Master"
     cat("Select the most recent Charge Description Master", fill = T)
@@ -22,14 +24,14 @@ generate_Premier_file <- function(df,start_date,end_date){
     dict_CDM <- read.csv(file = path_dict_CDM, na.strings = c("", "Unavailable"))
     dict_CDM <- subset(dict_CDM, select = c("CHARGE_CODE", "IPTB_cpt4"))
     dict_CDM <- na.omit(dict_CDM)
-    #colnames(dict_cdm)<- c("ChargeCode", "CPTCode")
+    colnames(dict_CDM)<- c("ChargeCode", "CPTCode")
     #Revenue to Cost Center 
     cat("Select the Revenue to Cost Center Mapping file", fill = T)
     path_dict_CC<- file.choose(new = T)
     dict_CC<- read.xlsx(path_dict_CC, sheetIndex = 1)
   #Merging dictionaries and data
-    premier_data <- merge.data.frame(premier_data, dict_CDM, all.x = T, all.y = F)
     premier_data <- merge.data.frame(premier_data, dict_CC, all.x = T, all.y = F)
+    premier_data <- merge.data.frame(premier_data, dict_CDM, all.x = T, all.y = F)
   return(premier_data)
 } 
 
