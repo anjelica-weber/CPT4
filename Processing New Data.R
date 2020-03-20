f1d5f56143369c5457074f1f4a374ed8feac410b
anjelica-weber <anjelica.weber@mssm.edu>
2020-03-20 13:43:04 -0400

    Working on analysis of charge data code section

--- Processing New Data.R
+++ Processing New Data.R
@@ -56,49 +56,49 @@ if(range_new_data[1]< range_master_data[2]){stop("The Posting Date range of the 
 # Quality Check - Volume Validation ---------------------------------------
   #Importing Dictionaries
     #Charge Description Master
-    cat("Select the most recent Charge Description Master", fill = T)
-    path_dict_CDM <- file.choose(new = T)
+    path_dict_CDM <- choose.files(caption= "Select the most recent Charge Description Master",multi = F)
     dict_CDM <- read.csv(file = path_dict_CDM, na.strings = c("", "Unavailable"))
     dict_CDM <- subset(dict_CDM, select = c("CHARGE_CODE", "IPTB_cpt4"))
     colnames(dict_CDM)<- c("ChargeCode", "CPTCode")
     #Revenue to Cost Center
-    cat("Select the Revenue to Cost Center Mapping file", fill = T)
-    path_dict_CC <- file.choose(new = T)
+    path_dict_CC <- choose.files(caption = 'Select the Revenue to Cost Center Mapping file', multi = F)
     dict_CC <- read.xlsx(path_dict_CC, sheetIndex = 1)
   #Preprocessing New Data
     #Adding CC and CPT codes
       CPT_data_analysis <- merge.data.frame(CPT_data_RAW, dict_CC, all.x = T, all.y = F)
       CPT_data_analysis <- merge.data.frame(CPT_data_analysis, dict_CDM, all.x = T, all.y = F)
     #Getting sum of charges per day per cost center
-      CPT_data_analysis_sum <- aggregate(CPT_data_analysis$NumberOfUnits, by= list(CPT_data_analysis$Premier.Facility.ID, CPT_data_analysis$Cost.Center, CPT_data_analysis$PostingDate), FUN= 'sum')
+      CPT_data_analysis_sum <- aggregate(CPT_data_analysis$NumberOfUnits, by= list(CPT_data_analysis$Premier.Facility.ID, CPT_data_analysis$Cost.Center, CPT_data_analysis$ServiceDate), FUN= 'sum')
       colnames(CPT_data_analysis_sum)  <- c('Facility ID', 'Cost Center', 'ServiceDate', 'Sum of Charges') 
   #Preprocessing Master Data
       master_data_analysis <- merge.data.frame(master_data_RAW, dict_CC, all.x = T, all.y = F)
       master_data_analysis <- merge.data.frame(master_data_analysis, dict_CDM, all.x = T, all.y = F)
       #Getting sum of charges per day per cost center
-      master_data_analysis_sum <- aggregate(master_data_analysis$NumberOfUnits, by= list(master_data_analysis$Premier.Facility.ID, master_data_analysis$Cost.Center, master_data_analysis$PostingDate), FUN= 'sum')
+      master_data_analysis_sum <- aggregate(master_data_analysis$NumberOfUnits, by= list(master_data_analysis$Premier.Facility.ID, master_data_analysis$Cost.Center, master_data_analysis$ServiceDate), FUN= 'sum')
       colnames(master_data_analysis_sum)  <- c('Facility ID', 'Cost Center', 'ServiceDate', 'Sum of Charges') 
   #Analysis of Data
 #analyze_charges <- function(master_df, new_df){
-  analyze_master <- as.data.frame(unique(master_data_analysis_sum$`Cost Center`))
+  analyze_master <- as.data.frame(unique(master_data_analysis_sum$`Cost Center`))#replace variable to function variable
   colnames(analyze_master) <- "Cost Center"
     for (i in 1:length(analyze_master$`Cost Center`)){
-          analyze_master$Mean[i] <- mean(master_data_analysis_sum[master_data_analysis_sum$`Cost Center`== analyze_master$`Cost Center`[i], 'Sum of Charges'])
-          analyze_master$SD[i] <- sd(master_data_analysis_sum[master_data_analysis_sum$`Cost Center`== analyze_master$`Cost Center`[i], 'Sum of Charges'])
+          analyze_master$Mean[i] <- mean(master_data_analysis_sum[master_data_analysis_sum$`Cost Center`== analyze_master$`Cost Center`[i], 'Sum of Charges'])#replace variable to function variable
+          analyze_master$SD[i] <- sd(master_data_analysis_sum[master_data_analysis_sum$`Cost Center`== analyze_master$`Cost Center`[i], 'Sum of Charges']) #replace variable to function variable
         }
-      analyze_new <- as.data.frame(unique(CPT_data_analysis_sum$`Cost Center`))
+      analyze_new <- as.data.frame(unique(CPT_data_analysis_sum$`Cost Center`))#replace variable to function variable
       colnames(analyze_new) <- "Cost Center"
     for(i in 1:length(analyze_new$`Cost Center`)){
-          analyze_new$`Mean New`[i] <- mean(CPT_data_analysis_sum[CPT_data_analysis_sum$`Cost Center`== analyze_new$`Cost Center`[i], 'Sum of Charges'])
-          analyze_new$`SD New`[i] <- sd(CPT_data_analysis_sum[CPT_data_analysis_sum$`Cost Center`== analyze_new$`Cost Center`[i], 'Sum of Charges'])
+          analyze_new$`Mean New`[i] <- mean(CPT_data_analysis_sum[CPT_data_analysis_sum$`Cost Center`== analyze_new$`Cost Center`[i], 'Sum of Charges'])#replace variable to function variable
+          analyze_new$`SD New`[i] <- sd(CPT_data_analysis_sum[CPT_data_analysis_sum$`Cost Center`== analyze_new$`Cost Center`[i], 'Sum of Charges'])#replace variable to function variable
         }
       if(length(analyze_master$`Cost Center`) != length(analyze_new$`Cost Center`)){
         missing_cc <- analyze_master[!(analyze_master$`Cost Center`  %in% analyze_new$`Cost Center`), 'Cost Center']
         cat(paste0("File uploaded is missing data for ", length(missing_cc), ' cost centers: '), fill = T)
         cat(paste(missing_cc), fill = T)
         stop("File is not accurate")
-      }else {
+      }else { #still working on this, what are criteria of charge volume to flag?
+        for (i in 1:length(CPT_data_analysis_sum$`Cost Center`)){
           
+        }
       }
 }
 
