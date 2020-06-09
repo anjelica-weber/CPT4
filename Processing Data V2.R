dff00c5e32c4ce6662f9849aac1d258f488a0e25
Weber <anjelica.weber@mssm.edu>
2020-06-09 15:34:39 -0400

    Saving changes

---
+++ Processing Data V2.R
@@ -0,0 +1,16 @@
+# Load Libraries ----------------------------------------------------------
+library(xlsx)
+library(tidyverse)
+
+
+# Importing RAW Data ------------------------------------------------------
+#Need to create new folder structure in share drive, CPT Data folder with sub folders by month (with all site data in folder)
+#dir_data <- 'J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Volume - Data'
+dir_data <- 'J:/deans/Presidents/SixSigma/Individual Folders/Current Employees/Engineers/Anjelica Weber/Projects/2020/Charge Detail R Coding/Mock Data Folder Structure'
+
+# QC ----------------------------------------------------------------------
+
+
+# Preprocessing Data ------------------------------------------------------
+
+
