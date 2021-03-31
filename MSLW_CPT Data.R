c0bec16713ad6b7ed784caf0e58a6f738e78e7a3
Weber <anjelica.weber@mssm.edu>
2021-03-31 15:49:37 -0400

    Cleaning code with Lintr package

--- MSLW_CPT Data.R
+++ MSLW_CPT Data.R
@@ -6,29 +6,44 @@ library(stringi)
 library(xlsx)
 
 # Constants ---------------------------------------------------------------
-start_date <- as.Date('2020-08-01')
-end_date <- as.Date('2020-08-31')
-dir_files <- 'J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Volume - Data/MSLW Data/SLW/Charge Detail/Source Data'
-dir_CDM <- 'J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Volume - Data/CDMs'
-dir_dictionary <- 'J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Volume - Data/MSLW Data/SLW/Charge Detail/Dictionaries/MSLW_Revenue to Cost Center Map.xlsx'
-dir_export <- 'J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Volume - Data/MSLW Data/SLW/Charge Detail'
+start_date <- as.Date("2020-12-20")
+end_date <- as.Date("2021-01-30")
+dir_files <- "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity
+            /Volume - Data/MSLW Data/Both Sites Data/Charge Detail/Source Data"
+dir_cdm <- "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity
+            /Volume - Data/CDMs"
+dir_dictionary <- "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity
+                  /Volume - Data/MSLW Data/Both Sites Data/Charge Detail
+                  /Dictionaries/MSLW_Revenue to Cost Center Map.xlsx"
+dir_export <- "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity
+                /Volume - Data/MSLW Data/Both Sites Data/Charge Detail"
 
 # Import Data -------------------------------------------------------------
-list_filenames_RAWdata <- list.files(path = choose.dir(caption = "Select most recent folder", default = dir_files), full.names = T, pattern = "csv$") # only pulls in .csv files
-if(length(list_filenames_RAWdata)%%3 != 0){stop("Unexpected number of files in selected folder")}#QC
-CPT_data <- lapply(list_filenames_RAWdata, read.csv)
+list_filenames_rawdata <- list.files(
+  path = choose.dir(caption = "Select most recent folder",
+                    default = dir_files),
+  full.names = T,
+  pattern = "csv$") # only pulls in .csv files
+if (length(list_filenames_rawdata) %% 3 != 0) {
+  stop("Unexpected number of files in selected folder")
+  }#QC
+cpt_data <- lapply(list_filenames_rawdata, read.csv)
 
 # Preprocess Data ---------------------------------------------------------
-CPT_data <- lapply(CPT_data, function(x) cSplit(x, colnames(x), sep = '|', type.convert = T)) # separating csvs into tables
-col_names_split <- function(df){
-  new_col_names<- unlist(stri_split_fixed(str = colnames(df)[1], '.'))
-  new_col_names<- sapply(new_col_names, function(x) stri_replace_all(str = x, regex = "_01", replacement = ""))
-  colnames(df)<- new_col_names
+cpt_data <- lapply(cpt_data, function(x)
+  cSplit(x, colnames(x), sep = "|", type.convert = T))
+  # separating csvs into tables
+col_names_split <- function(df) {
+  new_col_names <- unlist(stri_split_fixed(str = colnames(df)[1], "."))
+  new_col_names <- sapply(new_col_names,
+                         function(x) stri_replace_all(str = x, regex = "_01",
+                                                      replacement = ""))
+  colnames(df) <- new_col_names
   return(df)
 }
-CPT_data <- lapply(CPT_data, function(x) col_names_split(x))
-CPT_data <- do.call(plyr::rbind.fill, CPT_data)
-CPT_data <- CPT_data %>%
+cpt_data <- lapply(cpt_data, function(x) col_names_split(x))
+cpt_data <- do.call(plyr::rbind.fill, cpt_data)
+cpt_data <- cpt_data %>%
   mutate(PostingDate = as.Date(PostingDate),
          ServiceDate = as.Date(ServiceDate),
          FacilityId = as.character(FacilityId),
@@ -36,69 +51,122 @@ CPT_data <- CPT_data %>%
          ChargeCode = as.character(ChargeCode))
 
 # Import Dictionaries -----------------------------------------------------
-dictionary_CC <- read.xlsx2(file = dir_dictionary , sheetIndex = 1)
-dictionary_CC <- dictionary_CC %>%
+dictionary_cc <- read.xlsx2(file = dir_dictionary, sheetIndex = 1)
+dictionary_cc <- dictionary_cc %>%
   mutate(Premier.Facility.ID = as.character(Premier.Facility.ID),
          FacilityId = as.character(FacilityId),
          RevenueCenter = as.character(RevenueCenter),
          Cost.Center = as.character(Cost.Center))
-import_recent_CDM <- function(site.cdm) {
+import_recent_cdm <- function(site_cdm) {
   #Compiling Data on Files
-  Name <- list.files(path = dir_CDM, full.names = F, pattern ="csv$")
-  Path <- list.files(path = dir_CDM, full.names = T, pattern ="csv$")
-  Site <- sapply(Name, function(x) unlist(str_split(x, pattern = "_"))[1])
-  Date <- sapply(Name, function(x) unlist(str_split(x, pattern = "_"))[4])
-  Type <- sapply(Name, function(x) unlist(str_split(x, pattern = "_"))[4])
+  name <- list.files(path = dir_cdm, full.names = F, pattern = "csv$")
+  path <- list.files(path = dir_cdm, full.names = T, pattern = "csv$")
+  site <- sapply(name, function(x) unlist(str_split(x, pattern = "_"))[1])
+  date_created <- sapply(name, function(x)
+    unlist(str_split(x, pattern = "_"))[4])
+  type <- sapply(name, function(x) unlist(str_split(x, pattern = "_"))[4])
   #Formatting Data
-  Date <- sapply(Date, function(x) substr(x,1, 9))
-  Date <- as.Date(Date,"%d%B%Y")
-  Type <- sapply(Type, function(x) substr(x,11, nchar(x)))
+  date_created <- sapply(date_created, function(x) substr(x, 1, 9))
+  date_created <- as.Date(date_created, "%d%B%Y")
+  type <- sapply(type, function(x) substr(x, 11, nchar(x)))
   #Creating Table of Data
-  files <- data.table::data.table(Name, Path, Site, Date, Type)
-  files <- files %>% arrange(desc(Date)) %>% filter(Site == site.cdm)
+  files <- data.table::data.table(name, path, site, date_created, type)
+  files <- files %>% arrange(desc(date_created)) %>% filter(site == site_cdm)
   #Selecting Most Recent File
-  file_import <- files[1,]
+  cdm_file_import <<- files[1, ]
   #Importing Data
-  data_import <- read.table(file_import$Path, sep = ',',header = T, na.strings = c("", "Unavailable", "VOIDV"),fill = T)
+  data_import <- read.csv(cdm_file_import$path, sep = ",", header = T,
+                          na.strings = c("", "Unavailable", "VOIDV"), fill = T)
   #Processing Data
-  data_export <- data_import %>% 
-    select(CHARGE_CODE,IPTB_cpt4,CHARGE_DESC) %>%
+  data_export <- data_import %>%
+    select(CHARGE_CODE, OPTB_cpt4, CHARGE_DESC) %>%
     rename(ChargeCode = CHARGE_CODE,
-           CPTCode = IPTB_cpt4) %>%
-    drop_na()
+           CPTCode = OPTB_cpt4)
   return(data_export)
 }
-dictionary_CDM <- import_recent_CDM("SLR")
-dictionary_CDM <- dictionary_CDM %>% 
+dictionary_cdm <- import_recent_cdm("SLR")
+dictionary_cdm <- dictionary_cdm %>%
   mutate(ChargeCode = as.character(ChargeCode),
-         CPTCode = as.character(CPTCode))
-#dictionary_CDM <- read.csv('SLR_OP_CDM_24AUG2020.csv', na.strings = c("", "Unavailable"))
-#dictionary_CDM$CHARGE_DESC <- NULL
-#colnames(dictionary_CDM)<- c("ChargeCode", "CPTCode")
+         CPTCode = as.character(CPTCode)) %>%
+  drop_na()
 
 # Formatting Data ---------------------------------------------------------
-CPT_data_upload <- left_join(CPT_data, dictionary_CC)
-CPT_data_upload <- left_join(CPT_data_upload, dictionary_CDM)
+cpt_data_upload <- left_join(cpt_data, dictionary_cc)
+cpt_data_upload <- left_join(cpt_data_upload, dictionary_cdm)
 
 # Creating Upload File ----------------------------------------------------
-file_upload <- function(cpt.data, site, start.date, end.date) {
-  data_upload <- CPT_data_upload %>%
-    filter(ServiceDate >= start.date,
-           ServiceDate <= end.date,
+file_upload <- function(cpt_data, site, start_date_, end_date_) {
+  data_upload <- cpt_data %>%
+    filter(ServiceDate >= start_date_,
+           ServiceDate <= end_date_,
            Premier.Facility.ID == site) %>%
     mutate(Corp = "729805",
            EndDate = ServiceDate,
            ServiceDate = format(ServiceDate, "%m/%d/%Y"),
            EndDate = format(EndDate, "%m/%d/%Y")) %>%
-    select(Corp, Premier.Facility.ID, Cost.Center, ServiceDate, EndDate, CPTCode, NumberOfUnits) %>%
-    group_by(Corp, Premier.Facility.ID, Cost.Center, ServiceDate, EndDate, CPTCode) %>%
-    summarise(Volume = sum(NumberOfUnits, na.rm=T)) %>%
+    select(Corp, Premier.Facility.ID, Cost.Center, ServiceDate, EndDate,
+           CPTCode, NumberOfUnits) %>%
+    group_by(Corp, Premier.Facility.ID, Cost.Center, ServiceDate, EndDate,
+             CPTCode) %>%
+    summarise(Volume = sum(NumberOfUnits, na.rm = T)) %>%
     mutate(Budget = 0) %>%
     drop_na()
 }
-MSW_upload <- file_upload(CPT_data_upload, 'NY2162', start_date, end_date)
-MSM_upload <- file_upload(CPT_data_upload, 'NY2163', start_date, end_date)
+msw_upload <- file_upload(cpt_data_upload, "NY2162", start_date, end_date)
+msm_upload <- file_upload(cpt_data_upload, "NY2163", start_date, end_date)
+
+# Quality Chart -----------------------------------------------------------
+cpt_data_upload$Cost.Center.Description <- forcats::fct_explicit_na(
+  cpt_data_upload$Cost.Center.Description, na_level = "Other")
+quality_chart <- cpt_data_upload %>%
+  ungroup() %>%
+  select(Premier.Facility.ID, Cost.Center, Cost.Center.Description, ServiceDate,
+         NumberOfUnits, PostingDate) %>%
+  filter(ServiceDate >= range(PostingDate)[1]) %>%
+  mutate(PostingDate = NULL) %>%
+  group_by(Premier.Facility.ID, Cost.Center, Cost.Center.Description,
+           ServiceDate) %>%
+  summarise(TotalCharges = sum(NumberOfUnits)) %>%
+  arrange(ServiceDate, desc(Premier.Facility.ID), Cost.Center) %>%
+  mutate(ServiceDate = format(ServiceDate, "%b-%Y")) %>%
+  pivot_wider(names_from = ServiceDate, values_from = TotalCharges)
 
 # Exporting Files ---------------------------------------------------------
-write.table(MSW_upload, file = paste0(dir_export, "/MSW_CPT_", format(as.Date(range(MSW_upload$ServiceDate)[1], format = '%m/%d/%Y'), '%d%b%y'),' to ',format(as.Date(range(MSW_upload$ServiceDate)[2], format = '%m/%d/%Y'), '%d%b%y'), '.csv'), sep = ',', row.names = F, col.names = F)
-write.table(MSM_upload, file = paste0(dir_export, "/MSM_CPT_", format(as.Date(range(MSM_upload$ServiceDate)[1], format = '%m/%d/%Y'), '%d%b%y'),' to ',format(as.Date(range(MSM_upload$ServiceDate)[2], format = '%m/%d/%Y'), '%d%b%y'), '.csv'), sep = ',', row.names = F, col.names = F)
+write.table(msw_upload,
+            file = paste0(dir_export,
+                          "/MSW_CPT_",
+                          format(as.Date(range(msw_upload$ServiceDate)[1],
+                                         format = "%m/%d/%Y"), "%d%b%y"),
+                          " to ",
+                          format(as.Date(range(msw_upload$ServiceDate)[2],
+                                         format = "%m/%d/%Y"), "%d%b%y"),
+                          ".csv"),
+            sep = ",", row.names = F, col.names = F)
+write.table(msm_upload,
+            file = paste0(dir_export,
+                          "/MSM_CPT_",
+                          format(as.Date(range(msm_upload$ServiceDate)[1],
+                                         format = "%m/%d/%Y"), "%d%b%y"),
+                          " to ",
+                          format(as.Date(range(msm_upload$ServiceDate)[2],
+                                         format = "%m/%d/%Y"), "%d%b%y"),
+                          ".csv"),
+            sep = ",", row.names = F, col.names = F)
+write.table(quality_chart,
+            file = paste0(dir_export,
+                          "/Quality Chart_",
+                          format(as.Date(range(cpt_data_upload$PostingDate)[1],
+                                         format = "%m/%d/%y"), "%d%b%y"),
+                          " to ",
+                          format(as.Date(range(cpt_data_upload$PostingDate)[2],
+                                         format = "%m/%d/%y"), "%d%b%y"),
+                          ".csv"),
+            sep = ",", row.names = F, col.names = T)
+write_rds(cpt_data_upload, paste0(dir_files,
+                                  "/Master_",
+                                  format(range(cpt_data_upload$PostingDate)[1],
+                                         "%d%b%y"),
+                                  " to ",
+                                  format(range(cpt_data_upload$PostingDate)[2],
+                                         "%d%b%y"),
+                                  ".RDS"))
